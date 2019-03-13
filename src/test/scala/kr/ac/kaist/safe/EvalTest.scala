ef7a9e537f740f82daeafbf603d9ccbd4763d3f7
jhnaldo <jhpjhp0223@gmail.com>
2019-03-13 23:31:10 +0900

    Added core tests from test262 and Implemented EvalTest

--- src/test/scala/kr/ac/kaist/safe/EvalTest.scala
+++ src/test/scala/kr/ac/kaist/safe/EvalTest.scala
@@ -13,25 +13,22 @@ package kr.ac.kaist.safe
 
 import java.io._
 import kr.ac.kaist.safe.phase._
+import kr.ac.kaist.safe.nodes.core.State
 import org.scalatest._
 import scala.util.Random.shuffle
 
 class EvalTest extends AnalyzeTest {
-  // tests for interpreter
-  def evalHelper(file: File): Unit = {
+  // registration
+  val evalTestDir = testDir + "core"
+  shuffle(walkTree(new File(evalTestDir))).foreach(file => {
     val filename = file.getName
     lazy val name = file.toString
     lazy val config = safeConfig.copy(fileNames = List(name))
-    if (jsFilter(filename)) {
-      test(s"[Eval] $filename") {
-        val safeConfig = config.copy(fileNames = List(name))
-        CmdEval(List("-silent", name))
-        ???
-      }
+    if (jsFilter(filename)) test(s"[Eval] $filename") {
+      assert(CmdEval(List("-silent", name)).isSuccess)
     }
-  }
-
-  // registration
-  val evalTestDir = testDir + "core"
-  shuffle(walkTree(new File(evalTestDir))).foreach(evalHelper(_))
+    else if (errFilter(filename)) test(s"[Eval] $filename") {
+      assert(CmdEval(List("-silent", name)).isFailure)
+    }
+  })
 }
